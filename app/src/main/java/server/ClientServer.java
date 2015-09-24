7f32fad3ccbe6d623f3131265c42313fe576fcea
Em3r50n <emersonoliveira1406@gmail.com>
2015-09-23 22:52:53 -0300

    Primeiro Commit (Emerson)

---
+++ app/src/main/java/server/ClientServer.java
@@ -0,0 +1,12 @@
+package server;
+
+/**
+ * Created by Emerson Oliveira on 23/09/15 at 22:43.
+ *
+ */
+public class ClientServer implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
