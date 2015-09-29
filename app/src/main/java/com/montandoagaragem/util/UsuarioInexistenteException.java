432363b5ea1faa6cb5f3673beb574a082cec4692
Em3r50n <emersonoliveira1406@gmail.com>
2015-09-29 18:52:27 -0300

    Login

---
+++ app/src/main/java/com/montandoagaragem/util/UsuarioInexistenteException.java
@@ -0,0 +1,12 @@
+package com.montandoagaragem.util;
+
+/**
+ * Created by Emerson Oliveira on 29/09/15 at 18:36.
+ *
+ */
+public class UsuarioInexistenteException extends Exception {
+
+    public UsuarioInexistenteException() {
+        super();
+    }
+}
